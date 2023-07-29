bd3a51516d82a43d9b047ec251fccdfbafafca38
Arian Racca <arianracca@gmail.com>
2023-07-28 22:29:14 -0300

    Update bootstrap navbar

---
+++ src/components/CustomNavbar.tsx
@@ -0,0 +1,20 @@
+// Navbar.tsx
+import { Navbar, Nav } from 'react-bootstrap';
+
+const CustomNavbar = () => {
+  return (
+    <Navbar bg="dark" expand="lg" variant="dark">
+      <Navbar.Toggle aria-controls="navbar-nav" />
+      <Navbar.Collapse id="navbar-nav">
+        <Nav className="mr-auto">
+          <Nav.Link href="#welcome-section">About</Nav.Link>
+          <Nav.Link href="#projects">Work</Nav.Link>
+          <Nav.Link href="PENDIENTE">Blog</Nav.Link>
+          <Nav.Link href="#contact">Contact</Nav.Link>
+        </Nav>
+      </Navbar.Collapse>
+    </Navbar>
+  );
+};
+
+export default CustomNavbar;
