089f3b0b9e0c2f6f11f9a17d7aaae064f8d0ed20
Arian Racca <arianracca@gmail.com>
2023-07-29 20:25:22 -0300

    Advance on CustomNavbar logo

--- src/components/CustomNavbar.tsx
+++ src/components/CustomNavbar.tsx
@@ -1,17 +1,27 @@
 import { Navbar, Nav } from 'react-bootstrap';
+import logo from '../assets/logo.png';
 
 const CustomNavbar = () => {
   return (
-    <Navbar expand="lg" variant="dark">
+    <Navbar expand="md" className="navbar">
       <Navbar.Toggle aria-controls="navbar-nav" />
       <Navbar.Collapse id="navbar-nav">
-        <Nav className="mr-auto">
+        <Nav className="ml-auto">
           <Nav.Link href="#welcome-section">About</Nav.Link>
           <Nav.Link href="#projects">Work</Nav.Link>
-          <Nav.Link href="PENDIENTE">Blog</Nav.Link>
           <Nav.Link href="#contact">Contact</Nav.Link>
+          <Nav.Link href="PENDIENTE">Blog</Nav.Link>
         </Nav>
       </Navbar.Collapse>
+      <Navbar.Brand href="/" className="align-items-start">
+        <img
+          src={logo}
+          id="navbar-log"
+          alt="Logo"
+          height="40"
+          className="d-inline-block align-top ml-3"
+        />
+      </Navbar.Brand>
     </Navbar>
   );
 };
