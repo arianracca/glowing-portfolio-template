7f82a78cda0bd9b3233163e47835e18e7653fb55
Arian Racca <arianracca@gmail.com>
2023-07-28 21:37:26 -0300

    Add footer

---
+++ src/components/Footer.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+const Footer: React.FC = () => {
+  return (
+    <footer>
+      <ul>
+        <li>
+          <a href="#">About</a>
+        </li>
+        <li>
+          <a href="#">Work</a>
+        </li>
+        <li>
+          <a href="#">Contact</a>
+        </li>
+      </ul>
+      <span>Arian Racca 2020.</span>
+    </footer>
+  );
+};
+
+export default Footer;
