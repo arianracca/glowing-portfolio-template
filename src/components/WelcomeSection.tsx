5d23433ce3bf9393ac1e92f9cf90e11dbae49276
Arian Racca <arianracca@gmail.com>
2023-07-31 08:54:02 -0300

    Fix styles

--- src/components/WelcomeSection.tsx
+++ src/components/WelcomeSection.tsx
@@ -4,13 +4,9 @@ const WelcomeSection: React.FC = () => {
   return (
     <section id="welcome-section" className="welcome-section">
       <div className="welcome-text">
-        <div className='align-left'>
-          <h2 className="welcome-subtitle">Hello, my name is</h2>
-        </div>
+        <h2 className="welcome-subtitle">Hello, my name is</h2>
         <h1>Arian Racca</h1>
-        <div className='align-right'>
-          <h2 className="welcome-subtitle">This is my Profile</h2>
-        </div>
+        <h2 className="welcome-subtitle">This is my Profile</h2>
       </div>
       <div className="social-links">
         <div aria-label="LinkedIn">
