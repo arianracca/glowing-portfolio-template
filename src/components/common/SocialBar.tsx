ff5b3857fc8814e5880edf23cfd2b48b64002ebc
Arian Racca <arianracca@gmail.com>
2023-08-05 13:26:57 -0300

    Fix title html on social bar

--- src/components/common/SocialBar.tsx
+++ src/components/common/SocialBar.tsx
@@ -5,21 +5,11 @@ export const SocialBar: React.FC = () => {
   return (
     
     <div className={styles["social-links"]}>
-        <div aria-label="LinkedIn">
         <SocialLinkComponent socialMedia="LinkedIn" link="https://www.linkedin.com/in/arianracca" />
-        </div>
-        <div aria-label="GitHub">
         <SocialLinkComponent socialMedia="GitHub" link="https://github.com/arianracca" />
-        </div>
-        <div aria-label="Email">
         <SocialLinkComponent socialMedia="Email" link="mailto:info@arianracca.ar" />
-        </div>
-        <div aria-label="WhatsApp">
         <SocialLinkComponent socialMedia="WhatsApp" link="https://wa.me/5493413555676/?text=I'm%20interested%20in%20your%20work!" />
-        </div>
-        <div aria-label="Download CV">
         <SocialLinkComponent socialMedia="Download CV" link="https://drive.google.com/file/d/1u62YhHn7AjGtYWBbCt5TK7FhOzZXSSJw/view?usp=drive_link" />
-        </div>
     </div>
   )
 }
--- src/components/common/SocialLinkComponent.tsx
+++ src/components/common/SocialLinkComponent.tsx
@@ -11,7 +11,7 @@ const SocialLinkComponent: React.FC<SocialLinkProps> = ({ socialMedia, link }) =
   const Icon = getIconForSocialMedia(socialMedia);
 
   return (
-    <a href={link} target="_blank" rel="noopener noreferrer" aria-label={socialMedia}>
+    <a href={link} target="_blank" rel="noopener noreferrer" aria-label={socialMedia} title={socialMedia}>
       <button className={styles["social-link-btn"]}>
         {Icon && <Icon />}
       </button>
