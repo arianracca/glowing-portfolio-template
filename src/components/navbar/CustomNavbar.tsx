ce1614bb3d4c24f9e79232779fc9916f91f98e3e
Arian Racca <arianracca@gmail.com>
2023-08-05 12:33:59 -0300

    Fix all component styles

--- src/components/navbar/CustomNavbar.tsx
+++ src/components/navbar/CustomNavbar.tsx
@@ -10,29 +10,32 @@ const CustomNavbar = () => {
     setIsMenuOpen(!isMenuOpen);
   };
 
-  const navbarClasses = cn('navbar', styles.navbar, 'flex-space-between', styles['flex-space-between']);
+  const navbarClasses = cn('navbar flex-space-between', styles.navbar, styles['flex-space-between']);
 
-  const brandClasses = cn('navbar-brand', styles['navbar-brand'], 'align-items-start', 'ml-3');
+  const navContentContainerSpaceBetween = cn('flex-space-between', styles['flex-space-between'])
 
-  const toggleClasses = cn('navbar-toggler', { 'active' : isMenuOpen }, {[styles.active] : isMenuOpen});
+  const logoImageClass = cn('d-inline-block align-top ml-3', styles['navbar-logo']);
+
+  const brandClasses = cn('align-items-start ml-3 navbar-brand', styles['navbar-brand']);
+
+  const toggleClasses = cn(styles['navbar-toggler'], { 'active' : isMenuOpen }, {[styles.active] : isMenuOpen});
 
   const togglerLineClasses = cn('navbar-toggler-line', styles['navbar-toggler-line'])
 
-  const navbarNavClasses = cn('navbar-nav', styles['navbar-nav'])
+  const navbarNavClasses = cn(styles['navbar-nav'])
 
-  const navCollapseClasses = cn('navbar-collapse', { show: isMenuOpen });
+  const navCollapseClasses = cn('navbar-collapse', styles['navbar-nav'], styles['navbar-collapse'], { show: isMenuOpen });
 
 
   return (
     <Navbar expand="md" className={navbarClasses}>
-      <div className={cn('flex-space-between', styles['flex-space-between'])}>
+      <div className={navContentContainerSpaceBetween}>
         <Navbar.Brand href="/" className={brandClasses}>
           <img
             src={logo}
-            id="navbar-logo"
             alt="Logo"
             height="40"
-            className="d-inline-block align-top ml-3"
+            className={logoImageClass}
           />
         </Navbar.Brand>
         <Navbar.Toggle
