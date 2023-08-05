04b66032785c94ebc9040f7a9ae4e8e14e95dbb9
Arian Racca <arianracca@gmail.com>
2023-08-05 12:33:31 -0300

    Fix contact styles

--- src/components/contact/ContactSection.tsx
+++ src/components/contact/ContactSection.tsx
@@ -3,9 +3,11 @@ import styles from './contact.module.css'
 
 const ContactSection: React.FC = () => {
   return (
-    <section id="contact" className={styles["contact-section"]}>
-      <h2 className={styles["contact-header"]}>Contact me</h2>
-      <SocialBar />
+    <section id="contact">
+      <div className={styles["contact-section"]}>
+        <h2 className={styles["contact-header"]}>Contact me</h2>
+        <SocialBar />
+      </div>
     </section>
   );
 };
